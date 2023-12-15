4f9b7d8e436968c3029f1d4f07d0e6a94aa51ce6
Fernando Warno <ferwar@hotmail.com>
2023-12-15 04:12:41 -0300

    seializers.py - models.py - urls.py

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    technology = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True) # auto_now_add=True means that this field will be automatically added when the object is created
+
+    def __str__(self):
+        return self.title
